061fc6d405c9faaaf783c2bcf5a4ce5fd78ddcb3
Renato Groffe <renato.groffe@yahoo.com.br>
2020-05-23 16:48:07 -0300

    Teste de build de nova imagem

--- SiteContagem/Pages/Index.cshtml.cs
+++ SiteContagem/Pages/Index.cshtml.cs
@@ -21,7 +21,7 @@ namespace SiteContagem.Pages
                 TempData["Local"] = _CONTADOR.Local;
                 TempData["Kernel"] = _CONTADOR.Kernel;
                 TempData["TargetFramework"] = _CONTADOR.TargetFramework;
-                TempData["MensagemFixa"] = "Teste";
+                TempData["MensagemFixa"] = "Docker + Azure DevOps";
                 TempData["MensagemVariavel"] = configuration["MensagemVariavel"];
             }            
         }
